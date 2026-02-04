ffac4444f00948ca75e1728de22a9bb789fdbd1e
Bryson T. <35676267+brys0@users.noreply.github.com>
2026-02-04 10:06:49 -0600

    tidy: remove deps from main class

--- src/main/java/org/uwgb/compsci330/termfinal/Main.java
+++ src/main/java/org/uwgb/compsci330/termfinal/Main.java
@@ -1,11 +1,5 @@
 package org.uwgb.compsci330.termfinal;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
 // We need to create a generic main class
 // That just calls our JavaFX class
 // due to runtime dependency issues.
