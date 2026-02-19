2bab6a0340b8a07835e5b4ad3daf8c35f2d07c35
Brys <35676267+brys0@users.noreply.github.com>
2026-02-19 10:52:33 -0600

    feat: Add generated documentation for user routes

---
+++ server/src/main/java/org/uwgb/compsci330/server/entity/Conversation.java
@@ -0,0 +1,15 @@
+package org.uwgb.compsci330.server.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+//@Entity
+//@Table
+//public class Conversation {
+//
+//    //TODO [Reverse Engineering] generate columns from DB
+//}
