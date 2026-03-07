c733622e3f1eb1c29a3e7f1fc563f22ef3916ff7
brys0 <35676267+brys0@users.noreply.github.com>
2026-03-07 15:03:34 -0600

    feat: Add text message helper class

---
+++ server/src/main/java/org/uwgb/compsci330/server/websocket/raw/RawServerMessage.java
@@ -0,0 +1,16 @@
+package org.uwgb.compsci330.server.websocket.raw;
+
+import org.springframework.web.socket.TextMessage;
+import org.uwgb.compsci330.server.websocket.dto.OutboundEvent;
+import tools.jackson.databind.ObjectMapper;
+
+public class RawServerMessage {
+    private final TextMessage message;
+    public RawServerMessage(ObjectMapper mapper, OutboundEvent event) {
+        this.message = new TextMessage(mapper.writeValueAsString(event));
+    }
+
+    public TextMessage getTextMessage() {
+        return this.message;
+    }
+}
