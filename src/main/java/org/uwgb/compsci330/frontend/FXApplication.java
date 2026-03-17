1c40fa65d3cc61c75d380f3377092e91e07ee14e
David-Neuber <drneuber@outlook.com>
2026-03-16 21:35:12 -0500

    Implemented the sign-in page and sign-up page.

--- src/main/java/org/uwgb/compsci330/frontend/FXApplication.java
+++ src/main/java/org/uwgb/compsci330/frontend/FXApplication.java
@@ -1,15 +1,8 @@
 package org.uwgb.compsci330.frontend;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
@@ -21,25 +14,11 @@ public class FXApplication extends Application {
     public void start(Stage stage) throws IOException {
         stage.setTitle("NAME HERE");
 
-        Parent settingsFXML = FXMLLoader.load(
-                getClass().getResource(
-                        "/xml/pages/settings/settings.fxml"
-                )
-        );
-        Scene settings = new Scene(settingsFXML);
-
-        Parent chatFXML = FXMLLoader.load(
-                getClass().getResource(
-                        "/xml/pages/chat/chat.fxml"
-                )
-        );
-        Scene chat = new Scene(chatFXML);
-
-        Parent loginFXML = FXMLLoader.load(
+        Parent loginFXML = new FXMLLoader(
                 getClass().getResource(
                         "/xml/pages/login/login.fxml"
                 )
-        );
+        ).load();
         Scene login = new Scene(loginFXML);
 
         stage.setScene(login);
--- src/main/java/org/uwgb/compsci330/frontend/loginController.java
+++ src/main/java/org/uwgb/compsci330/frontend/loginController.java
@@ -1,4 +1,52 @@
 package org.uwgb.compsci330.frontend;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class loginController {
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    void signIn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUp(ActionEvent event) throws IOException {
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+
+        Parent signUpFXML = new FXMLLoader(
+                getClass().getResource(
+                        "/xml/pages/login/signUp.fxml"
+                )
+        ).load();
+
+        Scene signUp = new Scene(signUpFXML);
+
+        stage.setScene(signUp);
+
+    }
+
 }
